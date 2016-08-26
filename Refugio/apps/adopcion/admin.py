08c3476598a963b92cd6b71bff4ba617447f919b
Leo CP <leo011093@hotmail.com>
2016-08-25 20:02:46 -0500

    adding -vacuna- and -mascota- to the admin

--- Refugio/apps/adopcion/admin.py
+++ Refugio/apps/adopcion/admin.py
@@ -1,3 +1,8 @@
+from __future__ import absolute_import
 from django.contrib import admin
+from apps.mascota.models import Vacuna, Mascota
 
 # Register your models here.
+
+admin.site.register(Vacuna)
+admin.site.register(Mascota)
