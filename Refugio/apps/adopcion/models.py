7c1f959c42de51b0de3ad0ed22bd54a4bec8568c
Leo CP <leo011093@hotmail.com>
2016-08-25 19:22:55 -0500

    adding the -Persona- class

--- Refugio/apps/adopcion/models.py
+++ Refugio/apps/adopcion/models.py
@@ -3,3 +3,16 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Persona(models.Model):
+	"""Clase que me permite generar una persona"""
+	nombre = models.CharField(max_length=50)
+	apellidos = models.CharField(max_length=70)
+	edad = models.IntegerField()
+	telefono = models.CharField(max_length=12)
+	email = models.EmailField()
+	domicilio = models.TextField()
+	
+	#def __init__(self, arg):
+	#	super(ClassName, self).__init__()
+	#	self.arg = arg
