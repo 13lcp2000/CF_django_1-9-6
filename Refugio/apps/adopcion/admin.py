f56f3d3f1a381aa34137682eb301457a0923e576
Leo CP <leo011093@hotmail.com>
2016-08-25 23:09:46 -0500

    correcting the -admin- file in -adoption- app

--- Refugio/apps/adopcion/admin.py
+++ Refugio/apps/adopcion/admin.py
@@ -1,8 +1,6 @@
 from __future__ import absolute_import
 from django.contrib import admin
-from apps.mascota.models import Vacuna, Mascota
-
+from apps.adopcion.models import Persona
 # Register your models here.
 
-admin.site.register(Vacuna)
-admin.site.register(Mascota)
+admin.site.register(Persona)
