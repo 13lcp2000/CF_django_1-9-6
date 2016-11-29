624a7bfa6fe6ad4780dc8a1b6ca8bdb4d6e34ba5
Leo CP <leo011093@hotmail.com>
2016-11-29 04:39:47 -0500

    adding the class based view -SolicitudCreate-

--- Refugio/apps/adopcion/views.py
+++ Refugio/apps/adopcion/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+from __future__ import unicode_literals
+from __future__ import absolute_import
+
+
+from django.http import HttpResponseRedirect #, HttpResponse
+from django.core.urlresolvers import reverse_lazy
+from django.views.generic import CreateView
+
+from apps.adopcion.models import Solicitud
+from apps.adopcion.forms import PersonaForm, SolicitudForm 
 
 # Create your views here.
+
+def index_adopcion(request):
+    return HttpResponse("This is the index_adopcion funtion working xD")
+
+
+class SolicitudCreate(CreateView):
+	model = Solicitud
+	template_name = 'adopcion/solicitud_form2.html'
+	form_class = SolicitudForm
+	second_form_class = PersonaForm
+	success_url = reverse_lazy('adopcion:solicitud_listar')
+
+	def get_context_data(self, **kwargs):
+		context = super(SolicitudCreate, self).get_context_data(**kwargs)
+		if 'form' not in context:
+			context['form'] = self.form_class(self.request.GET)
+		if 'form2' not in context:
+			context['form2'] = self.second_form_class(self.request.GET)
+		return context 
+
+	def post(self, request, *args, **kwargs):
+		self.object = self.get_object 
+		form  = self.form_class(request.POST)
+		form2 = self.second_form_class(request.POST)
+		if form.is_valid() and form2.is_valid():
+			solicitud = form.save(commit=False)
+			solicitud.persona = form2.save()
+			solicitud.save()
+			return HttpResponseRedirect(self.get_success_url())
+		else:
+			return self.render_to_response(self.get_context_data(form=form, form2=form2))
+
