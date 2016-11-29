432277198e2af9e4f10058dd267cfe756004c591
Leo CP <leo011093@hotmail.com>
2016-11-29 05:33:34 -0500

    adding the -templates- directory

--- Refugio/Refugio/settings.py
+++ Refugio/Refugio/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'Refugio.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -98,9 +98,9 @@ AUTH_PASSWORD_VALIDATORS = [
     #{
     #    'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
     #},
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
+    #{
+    #    'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    #},
     #{
     #    'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     #},
@@ -125,3 +125,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
