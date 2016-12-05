8beb5af9b2dce21ec4457802003aebb01aba74d6
Leo CP <leo011093@hotmail.com>
2016-12-05 07:38:30 -0500

    adding the -urls- file for usuario app

---
+++ Refugio/apps/usuario/urls.py
@@ -0,0 +1,11 @@
+from __future__ import unicode_literals 
+from __future__ import absolute_import
+
+from django.conf.urls import url
+
+from apps.usuario.views import RegistroUsuario
+
+urlpatterns = [
+	url(r'^registrar', RegistroUsuario.as_view(), name="registrar")
+
+]
